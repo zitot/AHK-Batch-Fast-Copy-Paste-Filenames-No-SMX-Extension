e4504aab592f963dde60bd787daa3ee940983e18
zitot <76794131+zitot@users.noreply.github.com>
2022-04-12 23:18:38 -1000

    Create __FORLOOP__.bat

---
+++ __FORLOOP__.bat
@@ -0,0 +1,18 @@
+@echo off
+cd graphics
+echo Warning. All open command prompts receive input
+echo If this window was not launched using copyfilenames.ahk, close this window and try again
+echo IMPORTANT  Press the ESCAPE key on your keyboard to close the Autohotkey script 
+echo.
+echo Press the [`] key to paste the current filename
+echo The script will copy the next filename for you
+echo FILENAME
+FOR %%v IN (*.smx) DO (
+echo %%~nv
+echo %%~nv | clip
+pause >nul
+)
+echo.
+echo Please press the ESC key to end the Autohotkey script
+echo. | clip
+pause
